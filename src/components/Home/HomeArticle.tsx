bb8b866084419d948a593ad4db5d476e43e8578b
yli <yli@bol.com>
2023-02-19 00:31:59 +0100

    feat: fix lighthouse metrics

--- src/components/Home/HomeArticle.tsx
+++ src/components/Home/HomeArticle.tsx
@@ -9,15 +9,20 @@ type Props = {
 export const HomeArticle = ({ article }: Props) => (
   <div className="flex flex-col gap-4 py-4 border-b font-mono">
     <Link to={`/article/${article.id}`} preventScrollReset={true}>
-      <img
-        className="aspect-video object-cover"
-        src={article.imageUrl}
-        alt={article.title}
-      />
+      <picture>
+        <img
+          className="aspect-video object-cover"
+          src={article.imageUrl}
+          alt={article.title}
+          width="1920"
+          height="1080"
+          loading="lazy"
+        />
+      </picture>
       <h2 className="mt-4 text-2xl">{article.title}</h2>
       <p className="mt-4 text-sm hidden lg:block">{article.summary}</p>
     </Link>
-    <p className="text-sm text-gray-400">
+    <p className="text-sm text-gray-500">
       {new Date(article.publishedAt).toLocaleString()}
     </p>
   </div>
